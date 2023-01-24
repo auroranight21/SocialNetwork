2fda2c1d3e408fb9f747c5d8141722d69ac83aba
Aurora Night <auroranight@MacBook-Pro-Aurora.local>
2023-01-24 15:49:38 +0200

    typescript

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+
+
+const App = () => {
+    return (
+        <div className="App">
+            <div className="app-wrapper-content">
+            </div>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import state from './redux/state'
+
+
+
+
+ReactDOM.render(
+    <App appState={state}/>,
+    document.getElementById('root')
+);
